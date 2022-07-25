56001c7492fe753ed4c305050c05fdae51a3a4c4
Lany Aragão <lanyaragao5@gmail.com>
2022-07-25 15:26:10 -0300

    Correções do projeto

--- WebAPIAspNetCore/StatusCode/Controllers/UsuarioController.cs
+++ WebAPIAspNetCore/StatusCode/Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StatusCode.Models;
 
 namespace StatusCode.Controllers
@@ -14,66 +15,96 @@ namespace StatusCode.Controllers
 
         public ActionResult<List<Usuario>> RequererTodos()
         {
-            return Ok();
+            var Usuarios = DbSistema.Usuario?.ToList();
+            return Ok(Usuarios);
         }
 
         [HttpGet("{Id}")]
         public ActionResult<Usuario> RequererUmPelaId(int Id)
         {
+            //Procurar o usuário pelo Id
             Usuario usuario = DbSistema.Usuario.Find(Id);
 
+            //Se o user for null, retorna not found
             if (usuario == null)
             {
                 return NotFound();
             }
+            //Se encontrar o usuário, força o status 200 e retorna a instância usuario
             else { return Ok(usuario); }
         }
 
         [HttpPost]
-        public ActionResult<Usuario> PublicarUm(Usuario Usuario)
+        public ActionResult PublicarUm(Usuario Usuario)
         {
-            //Usuario usuario = DbSistema.Usuario.Find(Usuario.Cpf);
 
-            if (string.IsNullOrEmpty(Usuario.Cpf))
+            if (!ExisteCpf(Usuario.Cpf))
             {
                 return Conflict();
             }
             else
             {
-                return Created(Usuario.Cpf, Usuario);
+                DbSistema.Usuario.Add(Usuario);
+                DbSistema.SaveChanges();
+                return CreatedAtAction("Publicado um usuario",new { id = Usuario.Id });
             }
         }
 
         [HttpDelete("{Id}")]
-        public ActionResult<Usuario> DeletarUmPelaId(int Id, Usuario Usuario)
+        public ActionResult DeletarUmPelaId(int Id, Usuario Usuario)
         {
-            if (Id == Usuario.Id)
+            if (!ExisteUser(Id))
             {
-                return Unauthorized();
+                return NotFound();
             }
-            else { return NotFound(); }
+            else 
+            { 
+                return Unauthorized(); 
+            }
         }
 
         [HttpPut("{Id}")]
         public ActionResult<Usuario> SubstituirUmPelaId(int Id, Usuario Usuario)
         {
-            if (Id == Usuario.Id)
-            {
-                return Ok();
-            }
-            else if (Id == null)
-            {
-                return NotFound();
-            }
-            else if (Id != Usuario.Id)
+            //Se o Id da instância do usuário for diferente do Id do usuario pesquisado, retorna BadRequest
+            if (Id != Usuario.Id)
             {
                 return BadRequest();
             }
-            else if (Usuario.Cpf == Usuario.Cpf)
+
+            //Substitui o valor da instância no banco de dados
+
+            DbSistema.Entry(Usuario).State = EntityState.Modified;
+
+            try
             {
-                return Conflict();
+                //Salvar alteração no Banco de dados
+                DbSistema.SaveChanges();
             }
-            else { return BadRequest(); }
+            catch(DbUpdateConcurrencyException)
+            {
+                //Caso não consiga salvar, verifico se o usuario existe chamando o método criado
+                if (ExisteUser(Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+                return 
+            }
         }
+
+        private bool ExisteCpf(string Cpf)
+        {
+            return DbSistema.Usuario.Any(Coluna => Coluna.Cpf == Cpf);
+        }
+
+        private bool ExisteUser(int Id)
+        {
+            return DbSistema.Usuario.Any(Coluna => Coluna.Id == Id);
+        }
+
     }
 }
